76a0971701f5fa34b450270463476c93b1300fd8
msq <2205961827@qq.com>
2025-05-07 14:40:42 +0800

    feature: add tailwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="h-40 w-40 border-red border-2 border-solid flex">
+      <span class="flex-none">横向</span>
+      <div class="flex-1 overflow-auto">
+        <div class="h-full w-80 bg-blue-300 flex">
+          <div class="w-20 bg-blue-500">1</div>
+          <div class="w-20 bg-blue-500">2</div>
+          <div class="w-20 bg-blue-500">3</div>
+          <div class="w-20 bg-blue-500">4</div>
+        </div>
+      </div>
+      <span class="flex-none">横向</span>
+    </div>
+    <ol>
+      <li>
+        现有技术方案为1954年由潘振声创作的儿童歌曲《春天在哪里》，收录于《中国儿童歌曲选集》；
+      </li>
+      <li>
+        现有技术方案为1980年中央人民广播电台《小喇叭》节目播出的同名儿歌改编版本；
+      </li>
+      <li>
+        现有技术方案为2001年人民音乐出版社出版的《经典儿歌100首》中记载的该歌曲标准乐谱
+      </li>
+    </ol>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
